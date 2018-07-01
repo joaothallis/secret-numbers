5e042df6f9dd214d5ef399f1406c298d01c5f306
João Thallis <joaothallis.developer@gmail.com>
2018-07-01 15:33:10 -0300

    create __generate_secret_number function

--- secret_numbers.py
+++ secret_numbers.py
@@ -1,4 +1,12 @@
+from random import randint
+
+
 # Functions
 def __welcome_message():
     """Welcome message displayed when you start the game"""
     print("Welcome to Secret Numbers.")
+
+
+def __generate_secret_number():
+    """Generate a random number between 1 and 10 for the player to guess"""
+    return randint(1, 10)
