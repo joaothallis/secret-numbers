d6d7a7fd0ef1b383c9135b4984ded187759e6f53
João Thallis <joaothallis.developer@gmail.com>
2018-07-01 15:34:58 -0300

    create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Secret Numbers',
+      version='0.1',
+      description='Game where the player must guess a random number',
+      url='http://github.com/joaothallis/secret-numbers',
+      author='João Thallis',
+      author_email='joaothallis.developer@gmail.com',
+      license='MIT',
+      packages=['secret_numbers'],
+      zip_safe=False)
